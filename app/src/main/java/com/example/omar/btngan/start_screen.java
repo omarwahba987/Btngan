abd9cab6507a7f4d94e5e30c32ca15c362eb8f40
omarwahba987 <omar_wahba2@yahoo.com>
2018-02-05 01:35:37 +0200

    first change

--- app/src/main/java/com/example/omar/btngan/start_screen.java
+++ app/src/main/java/com/example/omar/btngan/start_screen.java
@@ -22,6 +22,8 @@ public class start_screen extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                //
+
             }
         });
     }
